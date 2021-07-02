5d2275907a299c9d1a2d7c61e94b837163f76a27
Kingq23 <80791284+Kingq23@users.noreply.github.com>
2021-07-02 13:08:43 -0400

    Update repl_cycles.py

--- repl_cycles/repl_cycles.py
+++ repl_cycles/repl_cycles.py
@@ -1,1 +1,12 @@
+from bs4 import BeautifulSoup 
+import requests,string
+def get_cycles(name):
+  url = "https://replit.com/@" + name
+  req = requests.get(url)
+  soup = BeautifulSoup(req.content,features="html5lib")
+  results = soup.find("span",class_="jsx-1979011775")
+  results = results.text
+  results = results.replace("(","")
+  results = results.replace(")","")
+  return results
 
